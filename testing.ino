f85a7c669f44e23a6c2f2644e8c3c6d6946f0000
arderenne <arderenne@gmail.com>
2016-09-23 09:33:53 -0400

    Update testing.ino

--- testing.ino
+++ testing.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_FRAM_SPI.h>
+#include <SPI.h>
 
 
 struct settings_t {
@@ -17,39 +18,56 @@ Adafruit_FRAM_SPI fram = Adafruit_FRAM_SPI(FRAM_CS);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  fram.begin)_;
-  //initSPI();
-}
-void initSPI(){
-  pinMode(FRAM_CS1, OUTPUT);
-  digitalWrite(FRAM_CS1, HIGH);
-  SPI.begin();
-  SPI.setDataMode(SPI_MODE0);  
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV2);
-  delay(5000);
+  fram.begin();
 }
 void loop() {
   // put your main code here, to run repeatedly:
   //int
   //if (fram.read(0, settings, (int)sizeof(settings))) Serial.println("worked");
-  settings.autoBrightness = true;
+
+  settings.localTimeZone = 5;
+  settings.localDST = false;
+  settings.localDisplayFormat24 = true;
+  settings.secondTimeZone = 0;
+  settings.secondDST = true;
+  settings.secondDisplayFormat24 = true;
+  settings.brightness = 6;
+  settings.autoBrightness = False;
+  Serial.println("Settings before writing");
+  Serial.print(settings.localTimeZone);Serial.print(" - ");
+  Serial.print(settings.localDST);Serial.print(" - ");
+  Serial.print(settings.localDisplayFormat24);Serial.print(" - ");
+  Serial.print(settings.secondTimeZone);Serial.print(" - ");
+  Serial.print(settings.secondDST);Serial.print(" - ");
+  Serial.print(settings.secondDisplayFormat24);Serial.print(" - ");
+  Serial.print(setting.brightness);Serial.print(" - ");
+  Serial.println(settings.autoBrightness);
   char buff[sizeof(settings)];
   memcpy(buff, &settings, sizeof(settings));
-  for (int i = 0;i < sizeof(settings);i++){
+  Serial.println("Byte form of Array to be written");
+  //write function
+  for (uint16_t i = 0;i < sizeof(settings);i++){
     Serial.print(buff[i]);
-    
+    fram.writeEnable(true);
+    fram.write8(i,buff[i]);
   }
-  
+  Serial.println("");
+  Serial.println("Byte form of array as read");
+  //read functin
+  for (int16_t i = 0; i < sizeof(settings); i++){
+    buff[i] = fram.read8[i];
+    serial.print(buff[i]);
+  }
   memcpy(&settings, buff, sizeof(settings));
-
-  Serial.print(settings.localTimeZone);
-  Serial.print(settings.localDST);
-  Serial.print(settings.localDisplayFormat24);
-  Serial.print(settings.secondTimeZone);
-  Serial.print(settings.secondDST);
-  Serial.print(settings.secondDisplayFormat24);
-  Serial.print(settings.brightness);
+  Serial.println("");
+  Serial.println("Settings after write and read");
+  Serial.print(settings.localTimeZone);Serial.print(" - ");
+  Serial.print(settings.localDST);Serial.print(" - ");
+  Serial.print(settings.localDisplayFormat24);Serial.print(" - ");
+  Serial.print(settings.secondTimeZone);Serial.print(" - ");
+  Serial.print(settings.secondDST);Serial.print(" - ");
+  Serial.print(settings.secondDisplayFormat24);Serial.print(" - ");
+  Serial.print(settings.brightness);Serial.print(" - ");
   Serial.println(settings.autoBrightness);
 	delay(5000);
 }
